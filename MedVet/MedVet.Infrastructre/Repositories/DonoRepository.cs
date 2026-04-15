7835d9d4448432208d4d80dcf52543af1eaa8081
JoaoHBatistaLeal <jhbatistaleal@gmail.com>
2026-04-14 22:35:16 -0300

    fix(repository): fixes misspelling 'interface' to 'class' in DonoRepository

--- MedVet/MedVet.Infrastructre/Repositories/DonoRepository.cs
+++ MedVet/MedVet.Infrastructre/Repositories/DonoRepository.cs
@@ -1,21 +1,60 @@
+using MedVet.Application.Interfaces.Repositories; // Importa a interface
+using MedVet.Domain.Entities;
+using MedVet.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using MedVet.Domain.Entities;
+using System.Linq;
 
-namespace MedVet.Application.Interfaces.Repositories;
-
-public interface IDonoRepository
+namespace MedVet.Infrastructure.Repositories;
+public class DonoRepository : IDonoRepository 
 {
-    IReadOnlyCollection<Dono> GetAll();
-    Dono? GetById(Guid id);
-    void Add(Dono dono);
-    
-    void Update(Dono dono);
-    
-    void Delete(Dono dono);
-    
-    void SaveChanges();
-    
-    Dono? GetByEmail(string email);
-    Dono? GetByIdWithPets(Guid id);
+    private readonly MedVetContext _context;
+
+    public DonoRepository(MedVetContext context)
+    {
+        _context = context;
+    }
+
+    public IReadOnlyCollection<Dono> GetAll()
+    {
+        return _context.Donos.ToList();
+    }
+
+    public Dono? GetById(Guid id)
+    {
+        return _context.Donos.FirstOrDefault(d => d.Id == id);
+    }
+
+    public void Add(Dono dono)
+    {
+        _context.Donos.Add(dono);
+    }
+
+    public void Update(Dono dono)
+    {
+        _context.Donos.Update(dono);
+    }
+
+    public void Delete(Dono dono)
+    {
+        _context.Donos.Remove(dono);
+    }
+
+    public void SaveChanges()
+    {
+        _context.SaveChanges();
+    }
+
+    public Dono? GetByEmail(string email)
+    {
+        return _context.Donos.FirstOrDefault(d => d.Email == email);
+    }
+
+    public Dono? GetByIdWithPets(Guid id)
+    {
+        return _context.Donos
+            .Include(d => d.Pets)
+            .FirstOrDefault(d => d.Id == id);
+    }
 }
